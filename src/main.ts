836a9f42e61169faa83cfc11879c4233f7933a9c
이일우 <leil@prismweaver.com>
2023-05-31 15:29:53 +0900

    vercel test2

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-
-BigInt.prototype['toJSON'] = function () {
-  return this.toString();
-};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.useGlobalPipes(new ValidationPipe());
-  const configService = app.get(ConfigService);
-  // const port = configService.get('PORT');
-  const frontendUrl = configService.get('FRONT_END_URL');
-
   await app.listen(3000);
-
-  app.enableCors({
-    credentials: true,
-    origin: [frontendUrl, 'https://studio.apollographql.com'],
-  });
-  console.log(
-    `[${
-      process.env.NODE_ENV
-    }] Application is running on: ${await app.getUrl()}`,
-  );
 }
 bootstrap();
