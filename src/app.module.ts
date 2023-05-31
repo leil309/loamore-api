72e6be2d7da728d318adf9faf33bea67c9abc1e3
이일우 <leil@prismweaver.com>
2023-05-31 16:04:47 +0900

    vercel test

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,28 +12,6 @@ import { ClassModule } from 'src/class/class.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: process.env.NODE_ENV === 'prod' ? '.env.prod' : '.env',
-    }),
-    GraphQLModule.forRootAsync<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-        sortSchema: true,
-        playground: false,
-        plugins: [ApolloServerPluginLandingPageLocalDefault()],
-        cors: {
-          origin: [
-            configService.get('FRONT_END_URL'),
-            'https://studio.apollographql.com',
-          ],
-          credentials: true,
-        },
-      }),
-      inject: [ConfigService],
-    }),
     PrismaModule,
     CharacterModule,
     ClassModule,
